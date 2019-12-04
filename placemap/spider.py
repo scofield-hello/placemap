a35a9617791b762aff39ed7b2dad75629b056b19
Nick <wecoding@126.com>
2019-12-04 15:47:31 +0800

    完善爬虫

--- placemap/spider.py
+++ placemap/spider.py
@@ -6,6 +6,7 @@ import time
 import queue
 import platform
 import threading
+from random import randint
 from typing import List, Dict, NewType, Tuple, Optional
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
@@ -13,16 +14,30 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webelement import WebElement
 from openpyxl import Workbook, load_workbook
 
+start_url = "https://placesmap.net/"
 Country = NewType("Country", Tuple[str, str])
 Area = NewType("Area", Tuple[str, str])
-Position = NewType("Position", Tuple[str, str, str, str, str, str, str])
+Position = NewType("Position", Tuple[str, str, str, str, str, str, str, str])
+xls_file_path = "./车站.xlsx"
 page_pattern = "<b>Found: ([0-9,]{1,10}) Places, ([0-9,]{1,10}) Pages</b>"
 p_xpath = '//div[@class="six columns"]/p'
-cordinate_pattern = r"([0-9\.]{3,}),\s+([0-9\.]{3,})"
-phone_pattern = r"Phone:\s?([+0-9\s]{3,30})"
-web_pattern = r"\((.*?)\)"
+cordinate_pattern = r"([0-9\.-]{3,}),\s+([0-9\.-]{3,})"
+phone_pattern = r"Phone:\s?((\(\d{1,4}\)\s?[0-9\s-]{3,30})|([+0-9\s-]{3,30}))"
+web_pattern = r"\(([a-zA-Z]{1,}.*)\)"
 process__queue = queue.Queue()
 is_finishing = False
+
+
+def start_crawl(**settings) -> None:
+    countrys: List[str] = settings["COUNTRY"]
+    matches: List[str] = settings["MATCHE"]
+    columns: List[str] = settings["COLUMN"]
+    process_task = __start_process_task(countrys, columns)
+    crawl_task = __start_crawl_task(countrys, matches)
+    crawl_task.start()
+    process_task.start()
+    process_task.join()
+    print("爬虫执行结束")
 
 
 def __start_crawl_task(countrys: List[str],
@@ -33,21 +48,26 @@ def __start_crawl_task(countrys: List[str],
     return thread
 
 
+def __init_chrome_dirver() -> WebDriver:
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver_path = "chromedriver.exe"
+    if platform.system() == "Darwin":
+        driver_path = "/Users/Nick/Dev/Python-In-Action/placemap/chromedriver"
+    chrome_driver = webdriver.Chrome(executable_path=driver_path,
+                                     options=chrome_options)
+    chrome_driver.set_page_load_timeout(120)
+    chrome_driver.set_script_timeout(60)
+    chrome_driver.implicitly_wait(30)
+    return chrome_driver
+
+
 def __init_crawl_job(countrys: List[str], matches: List[str]) -> None:
     chrome_driver = None
     try:
         print("start crawl data ......")
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        placemap_url = "https://placesmap.net/"
-        driver_path = "chromedriver.exe"
-        if platform.system() == "Darwin":
-            driver_path = "/Users/Nick/Dev/Python-In-Action/placemap/chromedriver"
-        chrome_driver = webdriver.Chrome(executable_path=driver_path,
-                                         options=chrome_options)
-        chrome_driver.set_page_load_timeout(90)
-        chrome_driver.set_script_timeout(5)
-        chrome_driver.get(placemap_url)
+        chrome_driver = __init_chrome_dirver()
+        chrome_driver.get(start_url)
         country_list: List[Country] = __parse_country_list(
             chrome_driver, countrys)
         for country in country_list:
@@ -71,14 +91,21 @@ def __init_crawl_job(countrys: List[str], matches: List[str]) -> None:
                     if match == None:
                         continue
                     page_size = int(match.group(2))
+                    # page_size = 3 if page_size > 3 else page_size
                     print(f"共有结果 [{page_size}] 页 ")
                     for page_num in range(1, page_size + 1):
                         p_el_list = __parse_taget_list(chrome_driver,
                                                        target_href, page_num)
                         for p_el in p_el_list:
-                            position = __parse_position(country_name, p_el)
-                            process__queue.put(position, False, 30)
-                            print("\n".join(position))
+                            try:
+                                position = __parse_position(
+                                    country_name, area_name, p_el)
+                                process__queue.put(position, False, 30)
+                                print("\n".join(position))
+                            except:
+                                print(sys.exc_info())
+                                continue
+
     except:
         print("爬虫执行异常=", sys.exc_info())
     finally:
@@ -86,18 +113,6 @@ def __init_crawl_job(countrys: List[str], matches: List[str]) -> None:
         is_finishing = True
         if chrome_driver is not None:
             chrome_driver.quit()
-
-
-def start_crawl(**settings) -> None:
-    countrys: List[str] = settings["COUNTRY"]
-    matches: List[str] = settings["MATCHE"]
-    columns: List[str] = settings["COLUMN"]
-    process_task = __start_process_task(countrys, columns)
-    crawl_task = __start_crawl_task(countrys, matches)
-    crawl_task.start()
-    process_task.start()
-    process_task.join()
-    print("爬虫执行结束")
 
 
 def __parse_country_list(chrome_driver: WebDriver,
@@ -120,7 +135,8 @@ def __parse_area_list(chrome_driver: WebDriver) -> List[Area]:
         map(lambda el: (el.text, el.get_attribute("href")), area_el_list))
 
 
-def __parse_position(country_name: str, p_el: WebElement) -> Position:
+def __parse_position(country_name: str, area_name: str,
+                     p_el: WebElement) -> Position:
     title = p_el.find_element_by_xpath("./b/a").text
     cordinate = p_el.find_element_by_xpath("./a").text
     cordinate_match = re.match(cordinate_pattern, cordinate)
@@ -133,7 +149,8 @@ def __parse_position(country_name: str, p_el: WebElement) -> Position:
     match_w = re.search(web_pattern, detail)
     phone = "" if not match_p else match_p.group(1).strip()
     web = "" if not match_w else match_w.group(1).strip()
-    return (country_name, title, address, latitude, longitude, phone, web)
+    return (country_name, area_name, title, address, latitude, longitude,
+            phone, web)
 
 
 def __parse_taget_list(chrome_driver: WebDriver, target_href: str,
@@ -148,8 +165,8 @@ def __parse_taget_list(chrome_driver: WebDriver, target_href: str,
 def __init_xls(country_list: List[str], columns: List[str]) -> Workbook:
     print("正在初始化表格...")
     workbook = None
-    if os.path.exists("./四国政府机构数据.xlsx"):
-        workbook = load_workbook('./四国政府机构数据.xlsx')
+    if os.path.exists(xls_file_path):
+        workbook = load_workbook(xls_file_path)
     else:
         workbook = Workbook()
         workbook.remove(workbook.get_active_sheet())
@@ -189,4 +206,4 @@ def __process_position(workbook: Workbook) -> None:
         while not process__queue.empty():
             position = process__queue.get()
             __write_xls(workbook, position)
-    __close_xls(workbook, "./四国政府机构数据.xlsx")
+    __close_xls(workbook, xls_file_path)
